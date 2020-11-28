dc574869bea92a0baacbefee4963bc055cc52cc0
deyems <adeyemisola11@gmail.com>
2020-11-28 23:35:45 +0100

    homepage

---
+++ public/homepage.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+<body>
+    <section>
+        <div>
+            <h2>
+                Welcome to Home page
+            </h2>
+        </div>
+    </section>
+    <section class="form-container">
+        <form action="" method="post">
+            <div>
+                <input type="text" name="firstname" id="">
+            </div>
+            <div>
+                <input type="email" name="email" id="">
+            </div>
+            <div>
+                <input type="number" name="phone" id="">
+            </div>
+            <div>
+                <button type="submit">Submit</button>
+            </div>
+        </form>
+    </section>
+</body>
+</html>
