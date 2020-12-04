d46766acbb6af6ebda7113cd763da955495f06b6
deyems <adeyemisola11@gmail.com>
2020-12-04 05:21:23 +0100

    removed unused lines

--- core/routes.php
+++ core/routes.php
@@ -3,8 +3,7 @@ use App\Router;
 
 Router::get('', 'HomeController@index');
 Router::get('auth/login', 'AuthController@login');
+Router::post('auth/login', 'AuthController@store');
 Router::put('auth/logout', 'AuthController@logout');
 Router::delete('auth/reg', 'AuthController@reg');
 Router::get('auth/branch', 'AuthController@login');
-
-Router::patch('auth/loggs', 'Controller@lob');
