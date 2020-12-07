019e44c56c221f0ce9d15d57509999cd5d8f97f7
deyems <adeyemisola11@gmail.com>
2020-12-07 23:17:00 +0100

    method to load post template

--- App/Controllers/SettingsController.php
+++ App/Controllers/SettingsController.php
@@ -5,4 +5,8 @@ class SettingsController extends BaseController{
     public function index(){
         $this->loadView('settings',['title' => 'Settings Dashboard', 'script' => 'settings.js']);
     }
+
+    public function post(){
+        $this->loadView('post',['title' => 'Post Dashboard', 'script' => 'post.js']);
+    }
 }
