1c6202b4067026038b649de56815cd46efa31fef
deyems <adeyemisola11@gmail.com>
2020-12-07 19:51:40 +0100

    added more routes for settings and Author

--- core/routes.php
+++ core/routes.php
@@ -1,9 +1,16 @@
 <?php
 use App\Router;
 
-Router::get('', 'HomeController@index');
+Router::get('/', 'HomeController@index');
 Router::get('auth/login', 'AuthController@login');
 Router::post('auth/login', 'AuthController@store');
+
 Router::put('auth/logout', 'AuthController@logout');
-Router::delete('auth/reg', 'AuthController@reg');
-Router::get('auth/branch', 'AuthController@login');
+
+Router::get('auth/reg', 'AuthController@register');
+Router::post('auth/reg', 'AuthController@register');
+
+Router::get('settings', 'SettingsController@index');
+Router::post('settings', 'SettingsController@index');
+
+Router::get('author', 'AuthorController@index');
