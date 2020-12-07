bcdcb6eac5affafaef2b30847d92512e91b65e71
deyems <adeyemisola11@gmail.com>
2020-12-07 19:50:11 +0100

    application entry point

--- public/index.php
+++ public/index.php
@@ -1,85 +1,11 @@
 <?php
-/*
-// echo "Welcome to our Program!". date('Y-m-d H:i:s');
-// echo "<pre>";
-// var_dump($_SERVER);
-// echo "</pre>";
 
-function getCleanURI(){
-    $uri = isset($_SERVER['PHP_SELF']) ? trim($_SERVER['PHP_SELF']) : '/';
-    // $uri = isset($_SERVER['REQUEST_URI']) ? trim($_SERVER['REQUEST_URI']) : '/';
-    $uri = str_ireplace('index.php', '', $uri);
-    $uri = trim($uri,'/');
-    return $uri;
-}
-
-function homePage(){
-    require_once "homepage.php";
-}
-
-function usersApi(){
-    header('Content-type: application/json');
-    echo json_encode(['status' => 'ok']);
-}
-
-$uri = getCleanURI();
-//ROUTING TO CREATE FRAMEWORK
-// switch ($uri) {
-//     case '':
-//         homePage();
-//         break;
-//     case 'about':
-//         echo 'About Page';
-//         break;
-//     case 'contact':
-//         header('Content-type: application/json');
-//         print_r(json_encode(['status' => 'ok']));
-//         break;
-//     case 'login':
-//         echo 'login Page';
-//         break;
-//     case 'api/users':
-//         // echo 'Users API';
-//         usersApi();
-//         break;
-//     default:
-//         die('404 NOT found');
-// }
-
-$routes = [
-    '' => 'homePage',
-    'api' => 'usersApi'
-];
-
-class HomepageController{
-    public static function index($greet){
-        echo "$greet Welcome to Index Really?";
-    }
-}
-$obj = new HomepageController();
-call_user_func(['HomepageController','index'],'Hi');
-
-
-foreach($routes as $url => $handler){
-    if($url === $uri){
-        call_user_func($handler);
-        exit;
-    }
-}
-*/
 
 /**
  * Entry Point
  */
 require_once '../vendor/autoload.php';
-// use App;
-// echo "Welcome<br />\n";
 
-// echo BASE_PATH;
-// echo "<br/>". PUBLIC_PATH;
 $app = new App\Application();
 $app->mount();
-// echo "\n";
-// Methods\Helpers::drive();
-// echo "\n";
 
