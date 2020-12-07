59ffdbeb9cb08e279a18d728b9178ca53f6bee4a
deyems <adeyemisola11@gmail.com>
2020-12-07 19:51:21 +0100

    partials

---
+++ views/partials/pagination.php
@@ -0,0 +1,20 @@
+<section class="navigation-bar">
+    <div></div>
+    <div class="pagination">
+        <div>
+            <a class="arrow-btn" href="">
+                <img src="uploads/images/leftarrow.svg" alt="left">
+            </a>
+        </div>
+        <div><a class="page-link active" href="">1</a></div>
+        <div><a class="page-link" href="">2</a></div>
+        <div><a class="page-link" href="">3</a></div>
+        <div><a class="page-link" href="">4</a></div>
+        <div><a class="page-link" href="">...</a></div>
+        <div>
+            <a class="arrow-btn" href="">
+                <img src="uploads/images/rightarrow.svg" alt="right">
+            </a>
+        </div>
+    </div>
+</section>
