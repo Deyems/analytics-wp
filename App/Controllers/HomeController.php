d7ff2aab90780edcb520af73c0136bbc02bc032d
deyems <adeyemisola11@gmail.com>
2020-12-09 13:30:20 +0100

    removed unused line

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -5,7 +5,8 @@ namespace App\Controllers;
  */
 class HomeController extends BaseController{
     public function index(){
-
+        // $db_host = env('DB_HOST');
+        // var_dump($db_host);
         $this->loadView('dashboard', ['title' => 'Dashboard','script' => 'dashboard.js']);
     }
 }
