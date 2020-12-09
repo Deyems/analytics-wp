34bc78caba87f2c9d09f7a0ce2071ce77c41c229
deyems <adeyemisola11@gmail.com>
2020-12-09 13:31:44 +0100

    User model

---
+++ App/Model/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Model;
+
+use App\Database\DB;
+/**
+ * Class User
+ *
+ * @package \App\Model
+ */
+class User {
+
+    protected static $table = 'users';
+
+    public static function create(array $data)
+    {
+        return DB::insert(static::$table, $data);
+    }
+
+    public static function findByEmail($email) {
+        $mysqli_result = DB::queryRaw('SELECT * FROM users WHERE email=%s', $email);
+        return $mysqli_result->fetch_object();
+    }
+
+    public static function updateLastLogin($id)
+    {
+       return  DB::query('UPDATE ' . static::$table . ' SET last_login =%s WHERE id = %d', date('Y-m-d H:i:s'), $id);
+    }
+}
