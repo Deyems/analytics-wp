27cf6c367c3a0b12bb2ab82422c27b6eccadeb16
deyems <adeyemisola11@gmail.com>
2020-12-09 13:30:03 +0100

    auth controller

--- App/Controllers/AuthController.php
+++ App/Controllers/AuthController.php
@@ -1,27 +1,69 @@
 <?php
 namespace App\Controllers;
 
-class AuthController extends BaseController{
-    public function login(){
-        if(isset($_GET['home'])){
-            $this->redirect('/');
-        }
+use App\Model\User;
+
+/**
+ * Class AuthController
+ *
+ * @package \App\Controllers
+ */
+class AuthController extends BaseController
+{
+
+    public function param($key) {
+        return isset($_GET[$key]) ? $_GET[$key] : '';
+    }
+    public function login() {
         $this->loadView('login', ['title' => 'Sign in']);
     }
 
-    public function register(){
-        $this->loadView('register', ['title' => 'Sign up']);
-    }
-    
-    public function forgot(){
-        $this->loadView('forgot', ['title' => 'Forgot Password']);
-    }
+    public function processLogin() {
 
-    public function store(){
         $email = $this->request->param('email');
         $password = $this->request->param('password');
-        var_dump($email, $password);
-        // print_r($_POST);
+
+        $user = User::findByEmail($email);
+        if(!$user)  {
+            return $this->redirect('/auth/login?msg=unable to login');
+        }
+
+        if(!password_verify($password, $user->password)) {
+            $this->redirect('/auth/login?msg=unable to login');
+        }
+
+        // set session
+        User::updateLastLogin($user->id);
+        $this->redirect('/');
     }
 
+
+    public function register()
+    {
+        $this->loadView('register', ['title' => 'Sign up']);
+    }
+
+
+    public function processReg()
+    {
+        $email = $this->request->param('email');
+        $password = $this->request->param('password');
+        $username = $this->request->param('username');
+
+        $result = User::create([
+            'email' => $email,
+            'password' => password_hash($password, PASSWORD_DEFAULT),
+            'name' => $username
+        ]);
+
+
+        if(!$result) {
+            //$this->request->setSession('errorMsg', 'unable to create user account');
+            $this->redirect('/auth/login?msg=error');
+            return;
+        }
+
+        $this->redirect('/auth/login?msg=success');
+
+    }
 }
