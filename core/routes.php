3aaf425e687768bdfae5b2c0fe97be874fdfc08b
deyems <adeyemisola11@gmail.com>
2020-12-10 09:22:43 +0100

    corrected the method to call for logout

--- core/routes.php
+++ core/routes.php
@@ -7,7 +7,7 @@ Router::get('/', 'HomeController@index');
 Router::get('auth/login', 'AuthController@login');
 Router::post('auth/login', 'AuthController@processLogin');
 
-Router::post('auth/logout', 'AuthController@login');
+Router::get('auth/logout', 'AuthController@logout');
 
 Router::get('auth/register', 'AuthController@register');
 Router::post('auth/register', 'AuthController@processReg');
