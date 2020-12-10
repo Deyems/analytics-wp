cd662de4d8bf0999a4d5b4c0fe3c01763f77ffea
deyems <adeyemisola11@gmail.com>
2020-12-10 09:20:06 +0100

    removed unsed lines and made a call to checkLogin from BaseController

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -3,10 +3,19 @@ namespace App\Controllers;
 /**
  * @package App\Controllers;
  */
-class HomeController extends BaseController{
+class HomeController extends BaseController
+{
+    //MiddleWare Stuff
+    // protected $requiresAuth = [
+    //     '/',
+    //     'auth/login'
+    // ];
     public function index(){
-        // $db_host = env('DB_HOST');
-        // var_dump($db_host);
+        // if(!isset($_SESSION['user_id']) || empty($_SESSION['user_id'])){
+        //     return $this->redirect('/auth/login');
+        // }
+        // var_dump($_SESSION['user_id']);
+        $this->checkLogin();
         $this->loadView('dashboard', ['title' => 'Dashboard','script' => 'dashboard.js']);
     }
 }
