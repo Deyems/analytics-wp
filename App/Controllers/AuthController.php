69a59965e328a9881558f99f70c1a0a4fd84679f
deyems <adeyemisola11@gmail.com>
2020-12-10 09:18:37 +0100

    process login and register with flash messages

--- App/Controllers/AuthController.php
+++ App/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 namespace App\Controllers;
 
 use App\Model\User;
+use App\Validators\Validation;
 
 /**
  * Class AuthController
@@ -11,59 +12,117 @@ use App\Model\User;
 class AuthController extends BaseController
 {
 
-    public function param($key) {
-        return isset($_GET[$key]) ? $_GET[$key] : '';
-    }
+    // public function param($key) {
+    //     return isset($_GET[$key]) ? $_GET[$key] : '';
+    // }
     public function login() {
         $this->loadView('login', ['title' => 'Sign in']);
     }
 
     public function processLogin() {
+        // try{
+            
+            $email = Validation::validateEmail($this->request->param('email'));
+            $password = $this->request->param('password');
 
-        $email = $this->request->param('email');
-        $password = $this->request->param('password');
+            if(!$email){
+                // throw new \Exception('Email is not valid');
+                $this->session->setFlashData('error', 'Email is invalid');
+            }
 
-        $user = User::findByEmail($email);
-        if(!$user)  {
-            return $this->redirect('/auth/login?msg=unable to login');
-        }
+            $user = User::findByEmail($email);
+            if(!$user)  {
+                $this->session->setFlashData('error', 'Unable to login');
+                return $this->redirect('/auth/login');
+            }
 
-        if(!password_verify($password, $user->password)) {
-            $this->redirect('/auth/login?msg=unable to login');
-        }
+            if(!password_verify($password, $user->password)) {
+                $this->session->setFlashData('error', 'Unable to login');
+                $this->redirect('/auth/login');
+            }
 
-        // set session
-        User::updateLastLogin($user->id);
-        $this->redirect('/');
+            // set session
+            User::updateLastLogin($user->id);
+            $this->session->setSessionData(USER_SESSION_KEY_NAME, $user->id);
+            $this->session->setFlashData('success', 'You are logged in');
+            $this->redirect('/');
+        // }catch(\Exception $e){
+            // throw new \Exception($e->getMessage());
+        // }
     }
 
 
     public function register()
     {
         $this->loadView('register', ['title' => 'Sign up']);
+        // echo "<br/>";
+        // foreach(User::findAll() as $row)
+        // {
+        //     echo "<pre>";
+        //         print_r($row);
+        //     echo "<pre/>";
+        // }
+        
     }
 
 
     public function processReg()
     {
-        $email = $this->request->param('email');
-        $password = $this->request->param('password');
-        $username = $this->request->param('username');
+        // try{
+            $email = Validation::validateEmail($this->request->param('email'));
+            $password = $this->request->param('password');
+            $username = Validation::validateNonRequiredField($this->request->param('username'));
+            $c_pass = $this->request->param('c_password');
 
-        $result = User::create([
-            'email' => $email,
-            'password' => password_hash($password, PASSWORD_DEFAULT),
-            'name' => $username
-        ]);
+            //Check for empty required Fields
+            $data = [
+                'email' => $email,
+                'password' => $password,
+                'c_password' => $c_pass
+            ];
 
+            if(!Validation::isAnyFieldEmpty($data)){
+                // throw new \Exception('Your fields cannot be empty');
+                $this->session->setFlashData('error', 'Your fields cannot be empty');
+            };
 
-        if(!$result) {
-            //$this->request->setSession('errorMsg', 'unable to create user account');
-            $this->redirect('/auth/login?msg=error');
-            return;
-        }
+            if(!$email){
+                // throw new \Exception('Email is not valid');
+                $this->session->setFlashData('error', 'Email is invalid');
+            }
 
-        $this->redirect('/auth/login?msg=success');
+            if(!Validation::checkPassword($password, $c_pass)){
+                // throw new \Exception('Passwords are not the SAME!');
+                $this->session->setFlashData('error', 'Passwords are not the SAME!');
+            }
 
+            $result = User::create([
+                'email' => $email,
+                'password' => password_hash($password, PASSWORD_DEFAULT),
+                'name' => $username,
+            ]);
+
+            if(!$result) {
+                $this->session->setFlashData('error', 'unable to create user account');
+                return $this->redirect('/auth/login');
+                // return;
+            }
+            $this->session->setFlashData('success', 'Account created successfully');
+            $this->redirect('/auth/login');
+
+        // }catch(\Exception $e){
+            // $this->redirect('/auth/login?msg='.$e->message());
+            // throw new \Exception($e->getMessage());
+        // }
+        
+    }
+    
+    public function forgot(){
+        $this->loadView('forgot', ['title' => 'Forgot Password']);
+    }
+
+    public function logout(){
+        $this->session->delete();
+        $this->redirect('/auth/login?msg=logged-out');
     }
 }
