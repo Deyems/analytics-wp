ab9371dd8db8992dc36fb9c4e64f206a7b621f93
deyems <adeyemisola11@gmail.com>
2020-12-10 09:19:06 +0100

    extended to check login state

--- App/Controllers/BaseController.php
+++ App/Controllers/BaseController.php
@@ -1,19 +1,25 @@
 <?php
 namespace App\Controllers;
 use App\Http\Request;
+use App\Http\Session;
 
 abstract class BaseController{
 
     protected $request;
+    protected $session;
 
     public function __construct(){
         $this->request = new Request();
+        $this->session = Session::getInstance();
+        
     }
 
     public function loadView($name, $data=[]){
+        $view = VIEW_PATH. $name. ".php";
+        if(!file_exists($view)) throw new \Exception($view .'not found');
         ob_start();
         extract($data);
-        require_once VIEW_PATH. $name. ".php";
+        require_once $view;
         $content = ob_get_contents();
         ob_clean();
         echo $content;
@@ -23,4 +29,14 @@ abstract class BaseController{
         header('Location: '. $to);
         exit;
     }
+
+    protected function checkLogin(){
+        $userId = $this->session->getSessionData(USER_SESSION_KEY_NAME);
+        if(!$userId){
+            $this->session->setFlashData('info', 'You must be logged in to continue');
+            $this->redirect('/auth/login');
+            exit;
+        }
+    }
+
 }
