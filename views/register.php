fdf113bd311278986b4f33e0c565cb7240eb9452
deyems <adeyemisola11@gmail.com>
2020-12-10 09:23:55 +0100

    added confirm password field

--- views/register.php
+++ views/register.php
@@ -9,12 +9,16 @@
             <h6 class="signin-subtitle">Please enter your credentials to proceed.</h6>
             <div class="form-group">
                 <label>Email address</label>
-                <input type="text" name="email" class="form-control" placeholder="samplemail@mail.com">
+                <input type="text" required name="email" class="form-control" placeholder="samplemail@mail.com">
                 <!-- <small><a href="/auth/forgot">Forgot password?</a></small> -->
             </div>
             <div class="form-group">
                 <label>Password</label>
-                <input type="password" name="password" class="form-control" >
+                <input type="password" required name="password" class="form-control" >
+            </div>
+            <div class="form-group">
+                <label>Confirm Password</label>
+                <input type="password" required name="c_password" class="form-control" >
             </div>
             <div class="form-group">
                 <label>Name</label>
