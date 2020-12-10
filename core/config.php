e753c1e1ecb79b0f72d30ca92d10509e2754283c
deyems <adeyemisola11@gmail.com>
2020-12-10 09:21:36 +0100

    added user-session-key constant

--- core/config.php
+++ core/config.php
@@ -4,3 +4,5 @@ define('BASE_PATH', dirname(__DIR__));
 define('PUBLIC_PATH', BASE_PATH.'/public/');
 define('VIEW_PATH', BASE_PATH.'/views/');
 define('DOT_ENV_FILE', BASE_PATH. '/.env');
+
+define('USER_SESSION_KEY_NAME', 'user_id');
