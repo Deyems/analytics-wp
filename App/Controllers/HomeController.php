22267d33e9f0dfc6cbf066dee9033308dab1aeea
deyems <adeyemisola11@gmail.com>
2021-02-11 16:11:01 +0100

    Home Controller to load dashboard

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -5,16 +5,8 @@ namespace App\Controllers;
  */
 class HomeController extends BaseController
 {
-    //MiddleWare Stuff
-    // protected $requiresAuth = [
-    //     '/',
-    //     'auth/login'
-    // ];
+    
     public function index(){
-        // if(!isset($_SESSION['user_id']) || empty($_SESSION['user_id'])){
-        //     return $this->redirect('/auth/login');
-        // }
-        // var_dump($_SESSION['user_id']);
         $this->checkLogin();
         $this->loadView('dashboard', ['title' => 'Dashboard','script' => 'dashboard.js']);
     }
